f405b129d78d029e455b5696eaa2a6171e434763
pr09eek <ps.prateek.sharma143@gamil.com>
2021-08-20 09:51:07 +0530

    Switched lvim to stable

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -14,19 +14,6 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- edit a default keymapping
 -- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
 
--- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
--- lvim.builtin.telescope.on_config_done = function()
---   local actions = require "telescope.actions"
---   -- for input mode
---   lvim.builtin.telescope.defaults.mappings.i["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-k>"] = actions.move_selection_previous
---   lvim.builtin.telescope.defaults.mappings.i["<C-n>"] = actions.cycle_history_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-p>"] = actions.cycle_history_prev
---   -- for normal mode
---   lvim.builtin.telescope.defaults.mappings.n["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.n["<C-k>"] = actions.move_selection_previous
--- end
-
 -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>lua require'telescope'.extensions.project.project{}<CR>", "Projects" }
 -- lvim.builtin.which_key.mappings["t"] = {
@@ -45,8 +32,6 @@ lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 0
-lvim.builtin.telescope.active = true
-lvim.builtin.project.active = true
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {}
@@ -62,21 +47,6 @@ lvim.builtin.treesitter.highlight.enabled = true
 --   end
 --   --Enable completion triggered by <c-x><c-o>
 --   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
--- you can overwrite the null_ls setup table (useful for setting the root_dir function)
--- lvim.lsp.null_ls.setup = {
---   root_dir = require("lspconfig").util.root_pattern("Makefile", ".git", "node_modules"),
--- }
--- or if you need something more advanced
--- lvim.lsp.null_ls.setup.root_dir = function(fname)
---   if vim.bo.filetype == "javascript" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "node_modules")(fname)
---       or require("lspconfig/util").path.dirname(fname)
---   elseif vim.bo.filetype == "php" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "composer.json")(fname) or vim.fn.getcwd()
---   else
---     return require("lspconfig/util").root_pattern("Makefile", ".git")(fname) or require("lspconfig/util").path.dirname(fname)
---   end
 -- end
 
 -- set a formatter if you want to override the default lsp one (if it exists)
@@ -95,20 +65,6 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- }
 
 -- Additional Plugins
-lvim.plugins = {
-    {"folke/tokyonight.nvim"},
-    {
-      "tpope/vim-eunuch"
-    },
-    {'kdheepak/lazygit.nvim'},
-    {
-      "windwp/nvim-ts-autotag",
-      event = "InsertEnter",
-    },
-    {
-      "p00f/nvim-ts-rainbow",
-    },
-}
 -- lvim.plugins = {
 --     {"folke/tokyonight.nvim"}, {
 --         "ray-x/lsp_signature.nvim",
@@ -116,6 +72,22 @@ lvim.plugins = {
 --         event = "InsertEnter"
 --     }
 -- }
+lvim.plugins = {
+  { "tpope/vim-eunuch" },
+  { "kdheepak/lazygit.nvim" },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+  },
+  { "p00f/nvim-ts-rainbow" },
+  {
+    "nvim-telescope/telescope-project.nvim",
+    event = "BufWinEnter",
+    setup = function()
+      vim.cmd [[packadd telescope.nvim]]
+    end,
+  },
+}
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- lvim.autocommands.custom_groups = {
